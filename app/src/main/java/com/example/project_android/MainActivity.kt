437744354ced9c587d2ad499fd93c22ced3a03c1
hoaiphongpvt <hoaiphongpvt@gmail.com>
2024-03-22 23:15:16 +0700

    Add Bottom Navigation

--- app/src/main/java/com/example/project_android/MainActivity.kt
+++ app/src/main/java/com/example/project_android/MainActivity.kt
@@ -2,39 +2,42 @@ package com.example.project_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.project_android.models.Movie
-import com.example.project_android.models.MovieRespone
-import com.example.project_android.services.MovieApiInterface
-import com.example.project_android.services.MovieApiServices
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.project_android.ui.fragment.DownloadsFragment
+import com.example.project_android.ui.fragment.HomeFragment
+import com.example.project_android.ui.fragment.SearchFragment
+import com.example.project_android.ui.fragment.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNav: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_form)
-//        val movieList = findViewById<RecyclerView>(R.id.rm_movies_list)
-//
-//        movieList.layoutManager = LinearLayoutManager(this)
-//        movieList.setHasFixedSize(true)
-//        getMovieData { movies: List<Movie> -> movieList.adapter = MovieAdapter(movies) }
-    }
+        setContentView(R.layout.activity_main)
+        bottomNav = findViewById(R.id.bottomNav)
 
-
-    private fun getMovieData(callback: (List<Movie>) -> Unit) {
-        val apiService = MovieApiServices.getInstance().create(MovieApiInterface::class.java)
-        apiService.getMovieList().enqueue(object : Callback<MovieRespone> {
-            override fun onResponse(call: Call<MovieRespone>, response: Response<MovieRespone>) {
-                return callback(response.body()!!.movie)
+        bottomNav.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.bottomHome -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_frame, HomeFragment()).commit()
+                    true // Trả về true để báo rằng sự kiện đã được xử lý
+                }
+                R.id.bottomSearch -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_frame, SearchFragment()).commit()
+                    true // Trả về true để báo rằng sự kiện đã được xử lý
+                }
+                R.id.bottomDownloads -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_frame, DownloadsFragment()).commit()
+                    true // Trả về true để báo rằng sự kiện đã được xử lý
+                }
+                R.id.bottomUser -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_frame, UserFragment()).commit()
+                    true // Trả về true để báo rằng sự kiện đã được xử lý
+                }
+                // Thêm các trường hợp cho các item khác nếu cần
+                else -> false
             }
-
-            override fun onFailure(call: Call<MovieRespone>, t: Throwable) {
-
-            }
-
-        })
+        }
     }
 }
