0386772dfeafdbc6409091a85c2ccec036207031
unknown <6919phong@gmail.com>
2024-04-02 14:06:39 +0700

    Download video

---
+++ app/src/main/java/com/example/project_android/ui/activity/VideoDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project_android.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.project_android.R
+
+class VideoDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_details)
+    }
+}
