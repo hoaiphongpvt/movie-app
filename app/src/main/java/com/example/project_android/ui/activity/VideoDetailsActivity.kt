d16e5acd90881bbae5922e41ba1d34f3c37aa630
hoaiphongpvt <hoaiphongpvt@gmail.com>
2024-04-18 01:14:15 +0700

    Revert "Merge pull request #43 from hoaiphongpvt/cphong"
    his reverts commit 9a9a8c91e41567f6e1ca93961d749c407874dc38, reversing
    changes made to 8a46d56c90169e6c015496220937b866cbe3f826.

--- app/src/main/java/com/example/project_android/ui/activity/VideoDetailsActivity.kt
+++ app/src/main/java/com/example/project_android/ui/activity/VideoDetailsActivity.kt
@@ -2,70 +2,52 @@ package com.example.project_android.ui.activity
 
 import android.os.Bundle
 import android.view.View
-import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.bumptech.glide.Glide
 import com.example.project_android.R
-import com.example.project_android.data.remote.YoutubeDataAPI
-import com.example.project_android.data.remote.TheMovieDatabaseAPI
 import com.example.project_android.data.services.AdsServices
-import com.example.project_android.viewmodel.CastDetailsViewModel
 import com.google.android.gms.ads.AdView
-import com.example.project_android.viewmodel.VideoViewModel
-
 
 class VideoDetailsActivity : AppCompatActivity() {
 
-    private lateinit var videoViewModel: VideoViewModel
     private lateinit var titlePage: TextView
-    private lateinit var videoDescription: TextView
+    private lateinit var btnBack: ImageButton
     private lateinit var mAdView: AdView
     private val adsServices = AdsServices(this)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_details)
 
         // Nhận dữ liệu từ Intent
         val videoName = intent.getStringExtra("videoName")
-        val videoID = intent.getStringExtra("videoKey")
-
-        videoViewModel = ViewModelProvider(this).get(VideoViewModel::class.java)
+        val videoKey = intent.getStringExtra("videoKey")
         titlePage = findViewById(R.id.video_title)
-        videoDescription = findViewById(R.id.video_description)
-        titlePage.text = videoName
+        btnBack = findViewById(R.id.backButton)
         mAdView = findViewById(R.id.adView)
+        titlePage.text = videoName
 
-        val frameVideo =
-            "<html><head><style>body, html { margin: 0; padding: 0; } iframe { width: 100%; height: 100%; border: none; } </style></head><body><iframe src=\"https://www.youtube.com/embed/$videoID\" frameborder=\"0\" allowfullscreen></iframe></body></html>"
+        val frameVideo = "<html><head><style>body, html { margin: 0; padding: 0; } iframe { width: 100%; height: 100%; border: none; } </style></head><body><iframe src=\"https://www.youtube.com/embed/$videoKey\" frameborder=\"0\" allowfullscreen></iframe></body></html>"
 
-        val displayYoutubeVideo = findViewById<WebView>(R.id.mWebView)
+        val displayYoutubeVideo = findViewById<View>(R.id.mWebView) as WebView
         displayYoutubeVideo.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                view?.loadUrl(request?.url.toString())
-                return true
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                return false
             }
-        }
-        if (videoID != null) {
-            videoViewModel.getVideoData(videoID) { videoDetails ->
-                if (videoDetails != null) {
-                    videoDescription.text = videoDetails
-                }
-            }
-        }
-        else {
-            videoDescription.text = "Description not Available"
         }
 
         val webSettings = displayYoutubeVideo.settings
         webSettings.javaScriptEnabled = true
         displayYoutubeVideo.loadData(frameVideo, "text/html", "utf-8")
 
+        btnBack.setOnClickListener {
+            onBackPressed()
+        }
+
         adsServices.loadBanner(mAdView,this)
     }
+
+
 }
